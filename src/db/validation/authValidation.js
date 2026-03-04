ad4700165b597e50a1d2079afdd2bbbb06c84916
hellvita <seredaolga03@gmail.com>
2026-03-04 19:56:06 +0200

    add custom error messages to the validation schemas

--- src/db/validation/authValidation.js
+++ src/db/validation/authValidation.js
@@ -1,12 +1,15 @@
 import { z } from 'zod';
 
 export const registerUserSchema = z.object({
-  email: z.email().toLowerCase(),
-  username: z.string().min(1).max(40),
-  password: z.string().min(6),
+  email: z.email({ required_error: 'Email is required' }).toLowerCase(),
+  username: z
+    .string({ required_error: 'Username is required' })
+    .min(1, 'Username must be at least 1 characters long')
+    .max(40, 'Username must be up to 40 characters long'),
+  password: z.string().min(6, 'Password must be at least 6 characters long'),
 });
 
 export const loginUserSchema = z.object({
-  email: z.email().toLowerCase(),
-  password: z.string(),
+  email: z.email({ required_error: 'Email is required' }).toLowerCase(),
+  password: z.string({ required_error: 'Password is required' }),
 });
--- src/db/validation/notesValidation.js
+++ src/db/validation/notesValidation.js
@@ -3,7 +3,10 @@ import { TAGS } from '../../constants/tags.js';
 import { z } from 'zod';
 
 export const createNoteSchema = z.object({
-  title: z.string().trim().min(1, 'Title is required'),
+  title: z
+    .string({ required_error: 'Title is required' })
+    .trim()
+    .min(1, 'Title must be at least 1 characters long'),
   content: z.string().trim().optional().default(''),
   tag: z.enum(TAGS).default('Todo'),
 });
@@ -14,6 +17,8 @@ export const updateNoteSchema = z
     content: z.string().trim().optional().optional(),
     tag: z.enum(TAGS).optional(),
   })
+  .strict()
   .refine((data) => Object.keys(data).length > 0, {
-    message: 'At least one field must be provided for update',
+    message:
+      "At least one field must be provided for update: 'title', 'content', 'tag'",
   });
