afbf72026420206742732218567056cb84cd3254
hellvita <seredaolga03@gmail.com>
2026-03-04 20:01:06 +0200

    add the try-catch blocks to catch the db errors, implement the getRefreshToken method

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -8,71 +8,83 @@ import {
   getSession,
   setSessionCookies,
 } from '../services/auth.js';
+import { handlePrismaError } from '../middleware/prismaErrorHandler.js';
+import { getRefreshToken } from '../helpers/tokens.js';
 
 export const registerUserController = async (req, res) => {
-  const newUser = await createUser(req.body);
+  try {
+    const newUser = await createUser(req.body);
 
-  if (!newUser) {
-    throw createHttpError(400);
+    if (!newUser) {
+      throw createHttpError(400, 'Invalid credentials');
+    }
+
+    const tokens = await createSession(newUser.id);
+
+    setSessionCookies(tokens, res);
+
+    res.status(201).json(newUser);
+  } catch (error) {
+    handlePrismaError(error);
   }
-
-  const tokens = await createSession(newUser.id);
-
-  setSessionCookies(tokens, res);
-
-  res.status(201).json(newUser);
 };
 
 export const loginUserController = async (req, res) => {
-  const { password } = req.body;
+  try {
+    if (!req.cookies.refreshToken) {
+      const { password } = req.body;
 
-  const user = await preLoginUser(req.body);
+      const user = await preLoginUser(req.body);
 
-  if (!user) {
-    throw createHttpError(401, 'Invalid credentials');
+      if (!user) {
+        throw createHttpError(401, 'Invalid credentials');
+      }
+
+      const isValidPassword = await bcrypt.compare(password, user.password);
+      if (!isValidPassword) {
+        throw createHttpError(401, 'Invalid credentials');
+      }
+
+      const tokens = await createSession(user.id);
+
+      setSessionCookies(tokens, res);
+
+      delete user.password;
+
+      res.status(200).json(user);
+    }
+
+    res.status(200).json({ message: 'User already logged in' });
+  } catch (error) {
+    handlePrismaError(error);
   }
-
-  const isValidPassword = await bcrypt.compare(password, user.password);
-  if (!isValidPassword) {
-    throw createHttpError(401, 'Invalid credentials');
-  }
-
-  const tokens = await createSession(user.id);
-
-  setSessionCookies(tokens, res);
-
-  res.status(200).json(user);
 };
 
 export const logoutUserController = async (req, res) => {
-  console.log('req.cookies: ', req.cookies);
-  console.log('req.cookie: ', req.cookie);
-  console.log('req: ', req);
+  try {
+    const refreshToken = getRefreshToken(req.cookies);
 
-  const { refreshToken } = req.cookies;
+    const session = await getSession(refreshToken);
 
-  const session = await getSession(refreshToken);
+    if (!session || session.expiresAt < new Date()) {
+      throw createHttpError(400, 'No active session found');
+    }
 
-  if (!session || session.expiresAt < new Date()) {
-    throw createHttpError(400, 'No active session found');
+    await deleteSessionByToken(refreshToken);
+
+    clearSessionCookies(res);
+
+    res.status(204).send();
+  } catch (error) {
+    handlePrismaError(error);
   }
-
-  await deleteSessionByToken(refreshToken);
-
-  clearSessionCookies(res);
-
-  res.status(204).send();
 };
 
 export const refreshSession = async (req, res, next) => {
   try {
-    const { refreshToken } = req.cookies;
+    const refreshToken = getRefreshToken(req.cookies);
 
-    if (!refreshToken) {
-      throw createHttpError(401, 'Unauthorized');
-    }
-
-    const session = await getSession(refreshToken);
+    const session = await getSession(refreshToken, req.userId);
 
     if (!session) {
       throw createHttpError(401, 'Session not found');
@@ -94,6 +106,6 @@ export const refreshSession = async (req, res, next) => {
       .status(200)
       .json({ success: true, message: 'Session refreshed' });
   } catch (error) {
-    next(error);
+    handlePrismaError(error);
   }
 };
