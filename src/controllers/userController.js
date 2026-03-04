6cdbccb9173ad05f7a5849c33c0d0543bc01853e
hellvita <seredaolga03@gmail.com>
2026-03-04 20:02:12 +0200

    add the getCurrentUserController method, add the try-catch blocks to catch the db errors, add route to get the user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,19 +1,28 @@
 import createHttpError from 'http-errors';
-import { deleteUserById } from '../services/user.js';
+import { deleteUserById, getUserById } from '../services/user.js';
 import { clearSessionCookies } from '../services/auth.js';
+import { handlePrismaError } from '../middleware/prismaErrorHandler.js';
+
+export const getCurrentUserController = async (req, res) => {
+  try {
+    const user = await getUserById(req.userId);
+
+    res.status(200).json(user);
+  } catch (error) {
+    handlePrismaError(error);
+  }
+};
 
 export const deleteUserByIdController = async (req, res) => {
-  if (!req.userId) throw createHttpError(404, 'User not found');
+  try {
+    if (!req.userId) throw createHttpError(404, 'User not found');
 
-  const deletedUser = await deleteUserById(req.userId);
+    const deletedUser = await deleteUserById(req.userId);
 
-  if (!deletedUser) throw createHttpError(404, 'User not found');
+    clearSessionCookies(res);
 
-  clearSessionCookies(res);
-
-  res.status(200).json({
-    id: deletedUser.id,
-    email: deletedUser.email,
-    username: deletedUser.username,
-  });
+    res.status(200).json(deletedUser);
+  } catch (error) {
+    handlePrismaError(error);
+  }
 };
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 
 import { authenticate } from '../middleware/authenticate.js';
-import { deleteUserByIdController } from '../controllers/userController.js';
+import {
+  deleteUserByIdController,
+  getCurrentUserController,
+} from '../controllers/userController.js';
 
 const router = Router();
+
+router.get('/user', authenticate, getCurrentUserController);
 
 router.delete('/user', authenticate, deleteUserByIdController);
 
